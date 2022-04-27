fec733761eadbe704d12f492ef8fe6fe44252f6d
Roma Babok <roman.babok18@gmail.com>
2022-04-27 06:37:38 +0300

    add

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,8 +15,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ 
 }
 
 module.exports = {
